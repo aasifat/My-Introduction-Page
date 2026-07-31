52637ebeabc873b72b1fca6dad528968cdf86c78
Sifat <ahsanahmedsifat@gmail.com>
2026-07-31 22:45:07 +0600

    Update portfolio image and hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,5 @@
 import { hero } from "../data/content.js";
 import Reveal from "./Reveal.jsx";
-
 export default function Hero() {
   return (
     <section id="home">
@@ -41,7 +40,7 @@ export default function Hero() {
 
         <Reveal delay={2} className="profile-card">
           <div className="profile-ph">
-            <img src="https://scontent.fdac24-5.fna.fbcdn.net/v/t39.30808-1/466465964_3422207871416291_5015784786576520712_n.jpg?stp=dst-jpg_tt6&cstp=mx958x960&ctp=s200x200&_nc_cat=101&ccb=1-7&_nc_sid=e99d92&_nc_eui2=AeFHtFIVm_5hUewatpB6VbDhYeEViD755NBh4RWIPvnk0A3bAPobZB_JAhX6_wn3nVSQWqndtZYvAqS2QsGcBkdM&_nc_ohc=ywXOG30S9HkQ7kNvwEfahy6&_nc_oc=Adp3LVe-KWkXKNGwU-seRyo_lZ7Gs9yOI4_Sjy_dyNKaqqWQEreCgg3oVpj7Ft-JieQ&_nc_zt=24&_nc_ht=scontent.fdac24-5.fna&_nc_gid=_kglCLOTzQt64gg9aiX1Ww&_nc_ss=7b2a8&oh=00_AQCmqOsOSCxkBfXsuPQgVUW7K_-WH99AInWlkrmuhtE-GA&oe=6A69860E" alt="Ahsan Ahmed Sifat" />
+            <img src={"/projects/IMG_5014.JPG"} alt="Ahsan Ahmed Sifat" />
           </div>
           <div className="profile-meta">
             <span>Role <b>CSE Student</b></span>
